39d3f9490d24c1a7784906030dafa0930dc60c27
ralphbarros <ralphbarros@gmail.com>
2022-08-08 19:26:31 -0300

    Criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './styles.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+       
+      <h1>Vitess + React</h1>
+     
+    </div>
+  )
+}
+
+export default App
